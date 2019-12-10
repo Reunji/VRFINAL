65d994e077e5a410b53a38a8d257cd982e1b16ba
Reunji <55166669+Reunji@users.noreply.github.com>
2019-12-10 18:11:25 -0500

    VR_FINAL

---
+++ Assets/WarningWallHit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningWallHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        this.GetComponent<MeshRenderer>().enabled = true;
+        this.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.yellow);
+
+    }
+}
